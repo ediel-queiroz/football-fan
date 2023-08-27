f10155277d67d4819c69b74cf47e39dde788fc2d
Ediel Queiroz <4809675+ediel-queiroz@users.noreply.github.com>
2023-08-27 14:36:22 +0100

    First commit

---
+++ src/test/java/com/github/ediel_queiroz/footballfan/footballfan/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ediel_queiroz.footballfan.footballfan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
